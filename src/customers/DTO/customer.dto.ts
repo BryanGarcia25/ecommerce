eeb7167bdeaddda11f3422d3c1736a3faf97c929
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 18:05:51 -0700

    feat: Crear DTO para customers

---
+++ src/customers/DTO/customer.dto.ts
@@ -0,0 +1,9 @@
+export class CustomerDto {
+    readonly name: string;
+    readonly email: string;
+    readonly phone: string;
+    readonly address: string;
+    readonly age: number;
+    readonly city: string;
+    readonly gender: string;
+}
