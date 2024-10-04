fd02efa6510b1e3974b8e1038644a65097dada42
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 19:34:24 -0700

    feat: Agregar validaciones en los campos del DTO

--- src/customers/DTO/customer.dto.ts
+++ src/customers/DTO/customer.dto.ts
@@ -1,9 +1,30 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator";
+
 export class CustomerDto {
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
+
+    @IsPhoneNumber()
     readonly phone: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly address: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly age: number;
+
+    @IsString()
+    @IsNotEmpty()
     readonly city: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly gender: string;
 }
