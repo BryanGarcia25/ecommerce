76dc1b8b2945418fc202b583d3e51e3e418c5264
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 02:01:50 -0700

    feat: Agregar información detallada de cada endpoint

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, ValidationPipe } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomerDto } from './DTO/customer.dto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('customers')
 @Controller('customers')
@@ -9,12 +9,15 @@ export class CustomersController {
     constructor(private readonly customersService: CustomersService) {}
 
     @Get()
+    @ApiOperation({ summary: 'Endpoint para obtener todos los clientes registrados' })
     @ApiResponse({ status: 200, description: 'Clientes obtenidos de la base de datos' })
     findAllCustomers() {
         return this.customersService.findAllCustomers();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Endpoint para obtener un cliente en específico mediante su id' })
+    @ApiParam({ name: 'id', description: 'ID del cliente' })
     @ApiResponse({ status: 200, description: 'Cliente obtenido de la base de datos' })
     @ApiResponse({ status: 404, description: 'El cliente no existe' })
     findCustomerById(@Param('id') id: string) {
@@ -22,12 +25,15 @@ export class CustomersController {
     }
 
     @Post()
+    @ApiOperation({ summary: 'Endpoint para registrar un cliente nuevo' })
     @ApiResponse({ status: 201, description: 'Cliente registrado con éxito' })
     createCustomer(@Body(new ValidationPipe()) customerDTO: CustomerDto) {
         return this.customersService.createCustomer(customerDTO);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Endpoint para actualizar un cliente en específico por medio de su id' })
+    @ApiParam({ name: 'id', description: 'ID del cliente' })
     @ApiResponse({ status: 201, description: 'Cliente actualizado con éxito' })
     @ApiResponse({ status: 404, description: 'El cliente no existe' })
     updateCustomer(@Param('id') id: string, @Body(new ValidationPipe()) customerDTO: CustomerDto) {
@@ -35,6 +41,8 @@ export class CustomersController {
     }
 
     @Delete('id')
+    @ApiOperation({ summary: 'Endpoint para eliminar un cliente en específico por medio de su id' })
+    @ApiParam({ name: 'id', description: 'ID del cliente' })
     @ApiResponse({ status: 201, description: 'Cliente eliminado con éxito' })
     @ApiResponse({ status: 404, description: 'El cliente no existe' })
     removeCustomer(@Param('id') id: string) {
