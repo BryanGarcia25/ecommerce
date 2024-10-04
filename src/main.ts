2ae66bde7779b92b582f69484ae41c8b0b20078e
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 00:38:56 -0700

    chore: Configurar Swagger para documentar la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configurar Swagger para la documentación de API
+  const config = new DocumentBuilder()
+    .setTitle('Servicio de Clientes')
+    .setDescription('API para gestionar clientes')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('API', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
