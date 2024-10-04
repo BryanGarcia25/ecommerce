c7ea0d7f176d73710023e6e9e614cf63b2bd956a
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 16:28:30 -0700

    test: Añadir prueba unitaria para el método de createCustomer de CustomersService

--- src/customers/customers.service.spec.ts
+++ src/customers/customers.service.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { CustomerDocument } from './customer.schema';
+import { Model } from 'mongoose';
 
 describe('CustomersService', () => {
   let service: CustomersService;
+  let model: Model<CustomerDocument>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [
+        CustomersService,
+        {
+          provide: getModelToken('Customer'),
+          useValue: {
+            find: jest.fn(),
+            create: jest.fn(),
+            findById: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndDelete: jest.fn()
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
+    model = module.get<Model<CustomerDocument>>(getModelToken('Customer'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Test para crear un cliente', async () => {
+    const customerDto = { name: 'Mary Gómez', email: 'mary@example.com', phone: '7381289384', address: 'Av. Springfield', age: 29, city: 'New York', gender: 'Fale' };
+
+    const mockCustomer = { id: '1', ...customerDto };
+
+    jest.spyOn(model, 'create').mockImplementation(() => mockCustomer as any);
+
+    const result = await service.createCustomer(customerDto);
+    expect(result).toEqual(mockCustomer)
   });
 });
