9cb9a46958d8fd30530f6a0874e274cedd32a18f
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 18:34:17 -0700

    feat: Crear controllador para customers

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CustomersService } from './customers.service';
+import { CustomerDto } from './DTO/customer.dto';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+    constructor(private readonly customersService: CustomersService) {}
+
+    @Get()
+    findAllCustomers() {
+        return this.customersService.findAllCustomers();
+    }
+
+    @Get(':id')
+    findCustomerById(@Param('id') id: string) {
+        return this.customersService.findCustomerById(id);
+    }
+
+    @Post()
+    createCustomer(@Body() customerDTO: CustomerDto) {
+        return this.customersService.createCustomer(customerDTO);
+    }
+
+    @Put(':id')
+    updateCustomer(@Param('id') id: string, @Body() customerDTO: CustomerDto) {
+        return this.customersService.updateCustomer(id, customerDTO);
+    }
+
+    @Delete('id')
+    removeCustomer(@Param('id') id: string) {
+        return this.customersService.removeCustomer(id);
+    }
+
+}
