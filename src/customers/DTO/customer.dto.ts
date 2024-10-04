9c088419b5b792eb9174db2d40bc4acce35e771d
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 01:16:31 -0700

    feat: Añadir etiqueta ApiProperty de Swagger

--- src/customers/DTO/customer.dto.ts
+++ src/customers/DTO/customer.dto.ts
@@ -1,29 +1,37 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator";
 
 export class CustomerDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly name: string;
 
+    @ApiProperty()
     @IsEmail()
     @IsNotEmpty()
     readonly email: string;
 
+    @ApiProperty()
     @IsPhoneNumber()
     readonly phone: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly address: string;
 
+    @ApiProperty()
     @IsNumber()
     @IsNotEmpty()
     readonly age: number;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly city: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly gender: string;
