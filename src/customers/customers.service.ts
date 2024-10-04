fc7313ddd1e0a9605291c6ffe0d5e0feeaa87a71
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 19:27:47 -0700

    feat: Implementar el esquema de Customer al servicio para interactuar con la base de datos

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,28 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CustomerDto } from './DTO/customer.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Customer, CustomerDocument } from './customer.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CustomersService {
-    constructor() {}
+    constructor(@InjectModel(Customer.name) private readonly customerDocumentModel: Model<CustomerDocument>) {}
 
     findAllCustomers() {
-
+        return this.customerDocumentModel.find().exec();
     }
 
-    findCustomerById(id: string) {
+    async findCustomerById(id: string) {
+        const customerFound = await this.customerDocumentModel.findById(id).exec();
 
+        if (!customerFound) {
+            return `El usuario con el id ${id} no existe en la base de datos`;
+        }
+
+        return customerFound;
     }
 
     createCustomer(customer: CustomerDto) {
-
+        const newCustomer = new this.customerDocumentModel(customer);
+        return newCustomer.save();
     }
 
-    updateCustomer(id: string, customer: CustomerDto) {
+    async updateCustomer(id: string, customer: CustomerDto) {
+        const customerFound = await this.customerDocumentModel.findByIdAndUpdate(id, customer, { new: true }).exec();
 
+        if (!customerFound) {
+            return `El usuario con el id ${id} no existe en la base de datos`;
+        }
+
+        return `Se actualizo el cliente con el id ${id} de manera correcta`;
     }
 
-    removeCustomer(id: string) {
+    async removeCustomer(id: string) {
+        const customerFound = await this.customerDocumentModel.findByIdAndDelete(id).exec();
 
+        if (!customerFound) {
+            return `El usuario con el id ${id} no existe en la base de datos`;
+        }
     }
     
 }
