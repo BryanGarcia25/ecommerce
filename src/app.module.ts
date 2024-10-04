fab3e853820b2166925d7e5abda76db2b1f73a19
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 18:46:24 -0700

    chore: Configurar mongoose en el módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    CustomersModule,
+    MongooseModule.forRoot(`mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
