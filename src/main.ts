8d5123aeceb3df354ecdd36f7eef3f9f17590aa8
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 14:40:21 -0700

    feat: Cargar archivo .env en main.ts
    Debido a que solamente se puede exportar como config se cambia el nombre de la constante para la documentación de API con swagger por documentBuilder, con la finalidad de evitar errores

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
   
   // Configurar Swagger para la documentación de API
-  const config = new DocumentBuilder()
+  const documentBuilder = new DocumentBuilder()
     .setTitle('Servicio de Clientes')
     .setDescription('API para gestionar clientes')
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, documentBuilder);
   SwaggerModule.setup('API', app, document);
   
   await app.listen(3000);
