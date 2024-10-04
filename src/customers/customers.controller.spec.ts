03dad45cb9c9e1e2cc26aa23f943909edd900dff
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 16:31:17 -0700

    test: Implementar prueba de integración para el método createCustomer de CustomersController
    Para verificar que el controllador esta llamando correctamente al servicio al momento de crear un cliente

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,18 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { CustomerDto } from './DTO/customer.dto';
 
 describe('CustomersController', () => {
   let controller: CustomersController;
+  let service: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
+      providers: [
+        CustomersService,
+        {
+          provide: CustomersService,
+          useValue: {
+            findAllCustomers: jest.fn(),
+            findCustomerById: jest.fn(),
+            createCustomer: jest.fn().mockResolvedValue({
+              id: 1, 
+              name: 'Mary Gómez',
+              email: 'mary@example.com',
+              phone: '7381289384',
+              address: 'Av. Springfield',
+              age: 29,
+              city: 'New York',
+              gender: 'Fale'                 
+            }),
+            updateCustomer: jest.fn(),
+            removeCustomer: jest.fn()
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<CustomersController>(CustomersController);
+    service = module.get<CustomersService>(CustomersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Creando un nuevo cliente', async () => {
+    const dto: CustomerDto = {
+      name: 'Mary Gómez',
+      email: 'mary@example.com',
+      phone: '7381289384',
+      address: 'Av. Springfield',
+      age: 29,
+      city: 'New York',
+      gender: 'Fale'
+    };
+
+    const result = await controller.createCustomer(dto);
+    expect(result).toEqual({
+      id: 1, 
+      name: 'Mary Gómez',
+      email: 'mary@example.com',
+      phone: '7381289384',
+      address: 'Av. Springfield',
+      age: 29,
+      city: 'New York',
+      gender: 'Fale'
+    });
+
+    expect(service.createCustomer).toHaveBeenCalledWith(dto);
+
   });
 });
