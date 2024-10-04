3e84b7523cbdff556d139d9b5f61365ed00df25b
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-03 18:33:56 -0700

    feat: Crear servicio para customers

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CustomerDto } from './DTO/customer.dto';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+    constructor() {}
+
+    findAllCustomers() {
+
+    }
+
+    findCustomerById(id: string) {
+
+    }
+
+    createCustomer(customer: CustomerDto) {
+
+    }
+
+    updateCustomer(id: string, customer: CustomerDto) {
+
+    }
+
+    removeCustomer(id: string) {
+
+    }
+    
+}
