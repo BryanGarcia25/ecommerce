272154bb7544ab0bda8b7d44805c1a85a5537eae
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 18:44:41 -0700

    feat: Crear método para registrar las acciones realizadas en la base de datos

--- src/audit/audit.service.ts
+++ src/audit/audit.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Audit } from './audit.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class AuditService {}
+export class AuditService {
+    constructor(@InjectModel(Audit.name) private readonly auditModel: Model<Audit>) {}
+
+    async logAction(action: string, entity: string, entityId: string, userId: string, changes: string) {
+        return this.auditModel.create(action, entity, entityId, userId, changes, Date())
+    }
+}
