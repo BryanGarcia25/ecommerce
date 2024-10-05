89bd8ce0a251f504fdb6303ac10291a7db7f9a12
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 18:34:51 -0700

    feat: Crear esquema de Mongoose para Audit

---
+++ src/audit/audit.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema({ timestamps: true })
+export class Audit extends Document {
+    @Prop({ required: true })
+    action: string;
+
+    @Prop({ required: true })
+    entity: string;
+
+    @Prop({ required: true })
+    entityId: string;
+
+    @Prop({ required: true })
+    userId: string;
+
+    @Prop({ required: true })
+    changes: string;
+
+    @Prop({ required: true })
+    timestamp: Date;
+}
+
+export const AuditSchema = SchemaFactory.createForClass(Audit);
