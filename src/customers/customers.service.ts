550282829f7d33fcd442409cd582a57a386cc11a
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-04 19:05:33 -0700

    feat: Agregar la auditoria en el servicio de customers

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -3,10 +3,14 @@ import { CustomerDto } from './DTO/customer.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Customer, CustomerDocument } from './customer.schema';
 import { Model } from 'mongoose';
+import { AuditService } from 'src/audit/audit.service';
 
 @Injectable()
 export class CustomersService {
-    constructor(@InjectModel(Customer.name) private readonly customerDocumentModel: Model<CustomerDocument>) {}
+    constructor(
+        @InjectModel(Customer.name) private readonly customerDocumentModel: Model<CustomerDocument>,
+        private readonly auditService: AuditService
+    ) {}
 
     findAllCustomers() {
         return this.customerDocumentModel.find();
@@ -22,26 +26,32 @@ export class CustomersService {
         return customerFound;
     }
 
-    createCustomer(customer: CustomerDto) {
-        return this.customerDocumentModel.create(customer);
+    async createCustomer(customer: CustomerDto, userId: string) {
+        const registeredClient = await this.customerDocumentModel.create(customer);
+        await this.auditService.logAction('create', 'customers', registeredClient._id as string, userId, JSON.stringify(customer))
+        return registeredClient;
     }
 
-    async updateCustomer(id: string, customer: CustomerDto) {
+    async updateCustomer(id: string, customer: CustomerDto, userId: string) {
         const customerFound = await this.customerDocumentModel.findByIdAndUpdate(id, customer, { new: true });
 
         if (!customerFound) {
             return `El usuario con el id ${id} no existe en la base de datos`;
         }
 
+        await this.auditService.logAction('update', 'customers', id, userId, JSON.stringify(customer))
+
         return `Se actualizo el cliente con el id ${id} de manera correcta`;
     }
 
-    async removeCustomer(id: string) {
+    async removeCustomer(id: string, userId: string) {
         const customerFound = await this.customerDocumentModel.findByIdAndDelete(id);
 
         if (!customerFound) {
             return `El usuario con el id ${id} no existe en la base de datos`;
         }
+
+        await this.auditService.logAction('delete', 'customers', id, userId, '{ }')
 
         return `Se elimino el cliente con el id ${id} de manera correcta`;
     }
