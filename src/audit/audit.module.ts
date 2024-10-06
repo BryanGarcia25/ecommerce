2ec8ebe622f806217eda8a159b8eddfb9ee15c1e
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-05 20:21:04 -0700

    feat: Añadir módulo de mongoose para crear el documento Audit

--- src/audit/audit.module.ts
+++ src/audit/audit.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuditService } from './audit.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Audit, AuditSchema } from './audit.schema';
 
 @Module({
-  providers: [AuditService]
+  imports: [
+    MongooseModule.forFeature(
+      [
+        {
+          name: Audit.name,
+          schema: AuditSchema
+        }
+      ]
+    )
+  ],
+  providers: [AuditService],
+  exports: [AuditService]
 })
 export class AuditModule {}
