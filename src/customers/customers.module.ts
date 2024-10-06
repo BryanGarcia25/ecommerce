fc927c857a26857d8ae7cb001c42533541bbac40
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-05 20:21:46 -0700

    update: Importar moódulo de Audit en el módulo de customers

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -3,9 +3,20 @@ import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Customer, CustomerSchema } from './customer.schema';
+import { AuditModule } from 'src/audit/audit.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Customer.name, schema: CustomerSchema }])],
+  imports: [
+    MongooseModule.forFeature(
+      [
+        { 
+          name: Customer.name, 
+          schema: CustomerSchema 
+        }
+      ]
+    ),
+    AuditModule
+  ],
   controllers: [CustomersController],
   providers: [CustomersService]
 })
