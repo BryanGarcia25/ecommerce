d6716837dc37118370c714957ae83c0b39df0305
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-07 14:40:14 -0700

    feat: Añadir importación de EventEmitter y CustomerCreateEvent

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -4,6 +4,8 @@ import { CustomersService } from './customers.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Customer, CustomerSchema } from './customer.schema';
 import { AuditModule } from 'src/audit/audit.module';
+import { CustomerCreateEvent } from './events/customer-create.event';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -15,9 +17,10 @@ import { AuditModule } from 'src/audit/audit.module';
         }
       ]
     ),
-    AuditModule
+    AuditModule,
+    EventEmitterModule.forRoot()
   ],
   controllers: [CustomersController],
-  providers: [CustomersService]
+  providers: [CustomersService, CustomerCreateEvent]
 })
 export class CustomersModule {}
