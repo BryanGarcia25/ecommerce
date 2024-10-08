3f82a3ff0164f0151bf08d5b9fac9524f36dfc93
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-08 15:04:57 -0700

    feat: Añadir datos de prueba para la collección de products

--- src/faker-database.ts
+++ src/faker-database.ts
@@ -35,4 +35,54 @@ async function createFakeCustomers() {
     }
 }
 
+async function createFakeProducts() {
+    config();
+    const uriMongoDb = `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`;
+    
+    const client = new MongoClient(uriMongoDb);
+
+    try {
+        await client.connect();
+        const db = client.db('eCommerce');
+        const productCollection = db.collection('products');
+        const products = [];
+
+        for (let index = 0; index < 30; index++) {
+            const stock = faker.number.int({ min: 0, max: 100 });
+            const status = stock > 0 ? 'Available' : 'Out of stock';
+            const category = faker.helpers.arrayElement(['Electronics', 'Toys', 'Clothing', 'Baby', 'Pets', 'Games', 'Home']);
+
+            const productName = faker.commerce.productName();
+            const productDescription = faker.commerce.productDescription();
+
+            const image = `https://picsum.photos/seed/${category}/640/480?grayscale&blur=2`
+
+
+            products.push({
+                name: productName,
+                description: productDescription,
+                category: category,
+                price: faker.commerce.price({ symbol: '$' }),
+                stock: stock,
+                image: image,
+                createdAt: faker.date.past(),
+                updatedAt: faker.date.recent(),
+                status: status,
+                sku: faker.string.alphanumeric(10),
+                discount: faker.number.int({ multipleOf: 10, min: 0, max: 50 }),
+                supplir: faker.person.fullName(),
+                stars: faker.number.float({ multipleOf: 0.5, min: 1, max: 5 })
+            });
+        }
+
+        const result = await productCollection.insertMany(products);
+        console.log(`${result.insertedCount} datos de prueba agregados exitosamente en la colección products`);
+        await client.close();
+
+    } catch (error) {
+        console.error(`Error al almacenar los datos de prueba en la colección de products ${error}`);
+    }
+}
+
 createFakeCustomers();
+createFakeProducts();
