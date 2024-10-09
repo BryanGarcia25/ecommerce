0fa83979ef0b0b805c6cb8e6c518dd7802aebec7
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-09 00:30:52 -0700

    feat: Importar el esquema y AuditModule en el módulo de orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schema/order.schema';
+import { AuditModule } from 'src/audit/audit.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [
+        {
+          name: Order.name,
+          schema: OrderSchema
+        }
+      ]
+    ),
+    AuditModule
+  ],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
