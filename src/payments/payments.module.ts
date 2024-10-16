53606acef7697961c364d541173bbe2b51c73d9a
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-15 17:21:08 -0700

    feat: Añadir módulo mongoose para el esquema de Payment

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './controller/payments.controller';
 import { PaymentsService } from './service/payments.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Payment, PaymentSchema } from './schema/payment.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [
+        {
+          name: Payment.name,
+          schema: PaymentSchema
+        }
+      ]
+    )
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService]
 })
