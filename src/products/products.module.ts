101d28989929964003cb30f1d661da1e959edbb6
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-16 01:01:59 -0700

    feat: Añadir módulos de auditoria y mongoose

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './controller/products.controller';
 import { ProductsService } from './service/products.service';
+import { AuditModule } from 'src/audit/audit.module';
+import { Product, ProductSchema } from './schema/product.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [
+        { 
+          name: Product.name, 
+          schema: ProductSchema 
+        }
+      ]
+    ),
+    AuditModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
