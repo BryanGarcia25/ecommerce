28cfced8a7f68b80fefadc1673b850e3cb95793b
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-17 01:28:01 -0700

    feat: Proveer PaypalPaymentStrategy en el módulo de Payments

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,6 +3,7 @@ import { PaymentsController } from './controller/payments.controller';
 import { PaymentsService } from './service/payments.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Payment, PaymentSchema } from './schema/payment.schema';
+import { PaypalPaymentStrategy } from './strategyPattern/paypal-payment-strategy';
 
 @Module({
   imports: [
@@ -13,9 +14,16 @@ import { Payment, PaymentSchema } from './schema/payment.schema';
           schema: PaymentSchema
         }
       ]
-    )
+    ),
   ],
   controllers: [PaymentsController],
-  providers: [PaymentsService]
+  providers: [
+    PaymentsService, 
+    {
+      provide: 'PAYMENT_STRATEGY',
+      useClass: PaypalPaymentStrategy
+    },
+    PaypalPaymentStrategy
+  ]
 })
 export class PaymentsModule {}
