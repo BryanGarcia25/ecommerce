9b4c3414898101f04ea9d6d5dd26cd8f3ecc41f2
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-17 01:27:08 -0700

    feat: Configurar controlador para recibir las solicitudes de pago

--- src/payments/controller/payments.controller.ts
+++ src/payments/controller/payments.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, ValidationPipe } from '@nestjs/common';
+import { PaymentsService } from '../service/payments.service';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PaymentDto } from '../dto/payment.dto';
 
+@ApiTags('customers')
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+    constructor(private readonly paymentService: PaymentsService) {}
+
+    @Post('create-order')
+    @ApiOperation({ summary: 'Endpoint para crear un pago en linea' })
+    @ApiResponse({ status: 201, description: 'Pago realizado' })
+    createOrder(@Body(new ValidationPipe()) paymentDto: PaymentDto) {
+        return this.paymentService.createOrder(paymentDto);
+    }
+
+    @Get('capture-payment/:orderId')
+    @ApiParam({ name: 'orderId', description: 'Id del pedido que se realiza la compra' })
+    @ApiOperation({ summary: 'Endpoint para capturar el pago realizado' })
+    capturePayment(@Query('token') token: string) {
+        return this.paymentService.capturePayment(token);
+    }
+}
