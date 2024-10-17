eafe6ccef31226e17b05648d1df464b78010dbca
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-10-17 01:26:31 -0700

    feat: Injectar PaymentStrategy y configurar servicio

--- src/payments/service/payments.service.ts
+++ src/payments/service/payments.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Payment, PaymentDocument } from '../schema/payment.schema';
+import { Model } from 'mongoose';
+import { PaymentDto } from '../dto/payment.dto';
+import { PaymentStrategy } from '../strategyPattern/payment-strategy.interface';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    constructor(
+        @InjectModel(Payment.name) private readonly paymentDocumentModel: Model<PaymentDocument>, 
+        @Inject('PAYMENT_STRATEGY') private readonly paymentStrategy: PaymentStrategy
+    ) {}
+
+    async createOrder(paymentDto: PaymentDto) {
+        this.paymentStrategy.createOrder(paymentDto);
+    }
+
+    async capturePayment(orderId: string) {
+        return this.paymentStrategy.capturePaymentForOrder(orderId);
+    }
+}
